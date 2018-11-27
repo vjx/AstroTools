c974677ace58ea243c53979eb88dff11db145a81
Xis <excalibas@gmail.com>
2018-11-27 14:22:57 +0000

    Updates

--- Carta.py
+++ Carta.py
@@ -4,6 +4,7 @@
 using flatlib from :João Ventura   
 
 """
+import dados
 import sys
 import datetime
 from flatlib import aspects
@@ -16,13 +17,7 @@ print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
 
 # Build a chart for a date and location
-data = datetime.datetime.utcnow()
-d = data.strftime('%Y/%m/%d')
-h = data.strftime('%H:%M:%S')
-date = Datetime(d,h)
-
-pos = GeoPos('38n72', '9w13')
-chart = Chart(date, pos)
+chart = Chart(dados.date, dados.pos)
 
 # Retrieve the Sun and Moon 
 sun = chart.get(const.SUN)
--- dados.py
+++ dados.py
@@ -1,1 +1,17 @@
+import sys
+import datetime
+from flatlib import aspects
+from flatlib import const
+from flatlib.chart import Chart
+from flatlib.datetime import Datetime
+from flatlib.geopos import GeoPos
+
+data = datetime.datetime.utcnow()
+d = data.strftime('%Y/%m/%d')
+h = data.strftime('%H:%M:%S')
+
+date = Datetime(d,h)
+
+pos = GeoPos('38n72', '9w13')
+
 
