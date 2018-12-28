7e77d9c92cf1ab778edd89ecd93674bebd260675
Xis <excalibas@gmail.com>
2018-12-28 02:18:24 +0000

    Updates

--- Carta.py
+++ Carta.py
@@ -13,21 +13,15 @@ from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
 
+#print ('Argument List:', str(sys.argv))
+print (dados.date, dados.pos)
+print()
 # Build a chart for a date and location
 chart = Chart(dados.date, dados.pos)
 
-# Retrieve the Sun and Moon 
-sun = chart.get(const.SUN)
-moon = chart.get(const.MOON)
 
-# Get the aspect
-aspect = aspects.getAspect(sun, moon, const.MAJOR_ASPECTS)
-print("Sol em",sun.sign,sun.signlon)
-print("Lua em",moon.sign,moon.signlon)
-#print(sun)
 #print(dir(sun))
-
+for obj in chart.objects:
+         print(obj)
 
--- dados.py
+++ dados.py
@@ -6,12 +6,22 @@ from flatlib.chart import Chart
 from flatlib.datetime import Datetime
 from flatlib.geopos import GeoPos
 
-data = datetime.datetime.utcnow()
-d = data.strftime('%Y/%m/%d')
-h = data.strftime('%H:%M:%S')
+if len(sys.argv) <= 1:
+    data = datetime.datetime.utcnow()
+    d = data.strftime('%Y/%m/%d')
+    h = data.strftime('%H:%M:%S')
+    date = Datetime(d,h)
+    pos = GeoPos('38n72', '9w13')
 
-date = Datetime(d,h)
+elif sys.argv[1] == "x":
+    date = Datetime('1981/05/22', '08:00', '+00:00')
+    pos = GeoPos('38n72', '9w00')
 
-pos = GeoPos('38n72', '9w13')
+    
+else:
+    date = Datetime('2015/03/13', '17:00', '+00:00')
+    pos = GeoPos('38n32', '8w54')
+
+
 
 
